03a08fe926cfd51ed41e32741a60030c016255d7
Samuel Alves <131412103+samu4alves@users.noreply.github.com>
2023-07-05 17:38:39 -0300

    Exercício002.py

--- Exercício002.py
+++ Exercício002.py
@@ -1,3 +1,3 @@
 print('Primeiramente, vou precisar saber seu nome. Por favor !')
 nome = input('Qual o seu nome ?')
-print(f'Prazer em te conhecer, {nome}!)
+print(f'Prazer em te conhecer, {nome}!')
