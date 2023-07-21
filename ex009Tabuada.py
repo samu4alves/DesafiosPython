075403e240c437050aa22d0a045b03accc666a63
Samuel Alves <131412103+samu4alves@users.noreply.github.com>
2023-07-21 05:01:09 -0300

    Tábuada Eletrônica
    Primeiramente duas apresentações. Uma por parte do usuário e outro do programa .
    Logo vem a instrução e depois os calculos.
    Então por útimo é feito agradecimentos.

---
+++ ex009Tabuada.py
@@ -0,0 +1,27 @@
+# APRESENTAÇÃO USUÁRIO
+print('SE APRESENTE, POR FAVOR !')
+nome = input('Qual seu nome ?:')
+br = ('-')
+print(br * 60)
+# APRESENTAÇÃO PROGRAMA
+print(f'Olá, {nome}. Tudo bem ? Me chamo MX04')
+# INSTRUÇÃO
+print('Digite um número, e mostrarei a tabuada dele !')
+print(br * 60)
+# CALCULO
+tabu = int(input('Digite um número:'))
+print(f'5 x 1  = {tabu * 1}')
+print(f'5 x 2  = {tabu * 2}')
+print(f'5 x 3  = {tabu * 3}')
+print(f'5 x 4  = {tabu * 4}')
+print(f'5 x 5  = {tabu * 5}')
+print(f'5 x 6  = {tabu * 6}')
+print(f'5 x 7  = {tabu * 7}')
+print(f'5 x 8  = {tabu * 8}')
+print(f'5 x 9  = {tabu * 9}')
+print(f'5 x 10 = {tabu * 10}')
+br2 = ('=')
+print(br2 * 60)
+# AGRADECIMENTO PARA O USUÁRIO
+print(f'Obrigado, {nome}. Por usar a tabuada !')
+print(br2 * 60)
