eeb09ca91ccb2ce90c3d5ca803ffbeaea205814d
Samuel Alves <131412103+samu4alves@users.noreply.github.com>
2023-07-21 04:56:36 -0300

    Conversor de Unidades Básico
    É solicitado o nome do usário para se apresentar, e logo o programa se apresenta também.
    Sendo digitado o valor do metro então é feito o calculo e convertido para centímetros e milimetros.
    Em seguida é agradecimento por usar o programa.

---
+++ ex008ConvUnidad.py
@@ -0,0 +1,14 @@
+ss = ('=')
+print(ss * 60)
+print('Conversor para metro')
+print(ss * 60)
+nome = input('Se apresente, por favor !. Qual seu nome ?:')
+print(f'Olá, {nome}. Tudo bem ? Me chamo MX04')
+metro = int(input('\nDigite o valor em metro, o número !:'))
+cm = metro * 100
+mm = metro * 1000
+print(f'\nAnalisei, então em {metro} metros, tem {cm} centímetros')
+print(f'E logo em {metro} metros, também pode ser {mm} milímetros')
+print(ss * 60)
+print(f'Obrigado {nome}, por usar o conversor de unidade !')
+print(ss * 60)
